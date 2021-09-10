329a0f5239890b800e58a26d51ec95d6ba8f0044
gorovaiakate <k-butuzova@mail.ru>
2021-09-10 12:38:33 +0600

    next changes

--- app/js/slider.js
+++ app/js/slider.js
@@ -5,16 +5,29 @@ let slides = sliderWrapp.querySelectorAll('.slide');
 let dots = document.getElementsByClassName('dot');
 let currentIndex = 1;
 let countSlide = 20;
+let width = -27;
 let timer;
 let isSliderMoving = false;
 
-sliderWrapp.style.marginLeft = -27 * (countSlide * currentIndex) + "px";
+if(window.innerWidth < 670){
+	width = -22;
+}
+if(window.innerWidth < 550){
+	width = -17;
+}
+if(window.innerWidth < 400){
+	width = -12;
+}
+
+sliderWrapp.style.marginLeft = width * (countSlide * currentIndex) + "px";
 dots[currentIndex - 1].classList.add('active');
 
 btnPrev.addEventListener('click', onShowPrevBtnClick);
 btnNext.addEventListener('click', onShowNextBtnClick);
 
 Array.from(dots).forEach((el, i) => el.setAttribute('data-key', i + 1));
+
+
 
 dots[0].parentElement.addEventListener('click', (e) => {
 	if (!e.target.classList.contains('dot') || e.target.classList.contains('active') || isSliderMoving) return false;
@@ -27,6 +40,7 @@ dots[0].parentElement.addEventListener('click', (e) => {
 	}
 	currentIndex = image_key;
 });
+
 
 function onShowPrevBtnClick() {
 	if (isSliderMoving) return false;
@@ -45,7 +59,7 @@ function sliderMove(pos) {
 	pos = pos === 'left' ? -1 : 1;
 	timer = setInterval(function () {
 		countSlide = countSlide + pos;
-		sliderWrapp.style.marginLeft = -27 * countSlide + "px";
+		sliderWrapp.style.marginLeft = width * countSlide + "px";
 
 		if (countSlide !== (20 * currentIndex)) return false;
 		clearInterval(timer);
@@ -61,11 +75,11 @@ function isLastOrFirst() {
 	if (currentIndex <= 0) {
 		countSlide = 20 * (slides.length - 2);
 		currentIndex = slides.length - 2;
-		sliderWrapp.style.marginLeft = -27 * countSlide + "px";
+		sliderWrapp.style.marginLeft = width * countSlide + "px";
 	}
 	if (currentIndex >= slides.length - 1) {
 		countSlide = 20;
 		currentIndex = 1;
-		sliderWrapp.style.marginLeft = -27 * countSlide + "px";
+		sliderWrapp.style.marginLeft = width * countSlide + "px";
 	}
 }
